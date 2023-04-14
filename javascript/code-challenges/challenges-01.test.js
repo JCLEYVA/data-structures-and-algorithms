facd0f145074f2d8147a3cb049bfafddf3b2e653
JEANETTE LEYVA <jeanettecleyva@outlook.com>
2023-04-13 20:44:23 -0600

    code challenge 1

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -1,11 +1,12 @@
-use strict';
+'use strict';
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 1
+
 Write a function named `addOne` that takes an array of numbers, and returns a new array of the numbers, incremented by 1.
+
 Use `forEach` to loop over the input array and work with each value.  Push the new value into a local array. Return the local array;
 ------------------------------------------------------------------------------------------------ */
-
 const addOne = (arr) => {
   const result = [];
   arr.forEach((num) => {
@@ -17,7 +18,9 @@ const addOne = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
+
 Write a function named `addExclamation` that takes an array of strings, and returns a new array of the same strings with an "!" added to the end.
+
 Use `forEach` to loop over the input array. Modify each string, and add the updated value into a local array. Return the local array;
 ------------------------------------------------------------------------------------------------ */
 
@@ -31,7 +34,9 @@ const addExclamation = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
+
 Write a function named `allUpperCase` that takes an array of strings, and returns a new array of the strings converted to upper case.
+
 Use `forEach` to loop over the input array. The modified strings should each be added into a local array. Return that local array.
 ------------------------------------------------------------------------------------------------ */
 
@@ -45,11 +50,13 @@ const allUpperCase = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
+
 Write a function named `greeting` that takes in a single string and returns the string in all uppercase letters, and followed by an "!".
+
 Then, write a function named `speaker` that takes in an array of strings and a callback function.
+
 Use `forEach` to build a new array of strings, each string modified by the callback. Return the new array.
 ------------------------------------------------------------------------------------------------ */
-
 const greeting = (word) => {
   return word.toUpperCase() + '!';
 };
@@ -62,18 +69,23 @@ const speaker = (words, callback) => {
   return result4;
 };
 
+
+
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
+
 Write a function named addValues that takes in an array and a value and pushes the value into the array. This function does not need a return statement.
+
 Then, write a function named addNumbers that takes in four arguments:
   - A number to be added to an array
   - An array into which the number should be added
   - The number of times the number should be added
   - A callback function to use to add the numbers to the array (Hint: you already defined it)
+
 Within the addNumbers function, invoke the callback function as many times as necessary, based on the third argument of the addNumbers function.
+
 Return the modified array.
 ------------------------------------------------------------------------------------------------ */
-
 const addValues = (arr, value) => {
   arr.push(value);
 };
@@ -85,9 +97,13 @@ const addNumbers = (num, arr, times, callback) => {
   return arr;
 };
 
+
 /* ------------------------------------------------------------------------------------------------
+
 CHALLENGE 6
+
 Write a function named createList that takes in an array of the current store inventory.
+
 The inventory is formatted like this:
 [
   { name: 'apples', available: true },
@@ -96,39 +112,55 @@ The inventory is formatted like this:
   { name: 'bananas', available: true },
   { name: 'blueberries', available: false }
 ]
+
 This function should use forEach to populate your grocery list based on the store's inventory. If the item is available, add it to your list. Return the final list.
 ------------------------------------------------------------------------------------------------ */
-
 const createList = (availableItems) => {
-  const groceryList = [];
-  availableItems.forEach((item) => {
-    if (item.available) {
-      groceryList.push(item.name);
-    }
-  });
-  return groceryList;
+  
 };
+
 
 /* ------------------------------------------------------------------------------------------------
 STRETCH - CHALLENGE 7
+
 Write a function named fizzbuzz that takes in an array of numbers.
+
 Iterate over the array using forEach to determine the output based on several rules:
   - If a number is divisible by 3, add the word "Fizz" to the output array.
   - If the number is divisible by 5, add the word "Buzz" to the output array.
   - If the number is divisible by both 3 and 5, add the phrase "Fizz Buzz" to the output array.
   - Otherwise, add the number to the output array.
+
 Return the resulting output array.
 ------------------------------------------------------------------------------------------------ */
 
 const fizzbuzz = (arr) => {
-  // Solution code here...
-};
+  const output = [];
+
+  arr.forEach((num) => {
+    if (num % 3 === 0 && num % 5 === 0) {
+      output.push("Fizz Buzz");
+    } else if (num % 3 === 0) {
+      output.push("Fizz");
+    } else if (num % 5 === 0) {
+      output.push("Buzz");
+    } else {
+      output.push(num);
+    }
+  });
+
+  return output;
+}
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
+
 All the code below will verify that your functions are working to solve the challenges.
+
 DO NOT CHANGE any of the below code.
+
 Run your tests from the console: jest challenges-01.test.js
+
 ------------------------------------------------------------------------------------------------ */
 
 describe('Testing challenge 1', () => {
